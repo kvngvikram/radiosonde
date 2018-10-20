864380a262a60041f7fceee2007a31b10b37c6ff
kvngvikram <happykvng@gmail.com>
2018-10-20 22:06:39 +0000

    correcting indents

--- radiosonde.py
+++ radiosonde.py
@@ -38,72 +38,72 @@ dat = soup.find("pre")
 #print(dat)
 
 if dat is None :
-    print('\n\n\n\tSomething went wrong dude.')
-    print('\n\tCheck if you have given the right dates.')
-    print('\n\tIf nothing works then call KVNG Vikram, and both of you search in google together.\n\n\n\n')
+	print('\n\n\n\tSomething went wrong dude.')
+	print('\n\tCheck if you have given the right dates.')
+	print('\n\tIf nothing works then call KVNG Vikram, and both of you search in google together.\n\n\n\n')
 
 else : 
-    # not mydat is a string
-    mydat = str(dat)
-    #print(mydat)
+	# not mydat is a string
+	mydat = str(dat)
+	#print(mydat)
 
-    # removing the headders and other characters so that only numbers are left 
-    mydat = mydat[318:len(mydat)-7]
-    #print(mydat)
+	# removing the headders and other characters so that only numbers are left 
+	mydat = mydat[318:len(mydat)-7]
+	#print(mydat)
 
-    # removing the last line of data as it can be incomplete
-    mydata = mydat[:len(mydat)-78]
-    #print(mydata)
+	# removing the last line of data as it can be incomplete
+	mydata = mydat[:len(mydat)-78]
+	#print(mydata)
 
-    lines = mydata.splitlines()
+	lines = mydata.splitlines()
 
-    p = np.array([])
-    h = np.array([])
-    t = np.array([])
-    td = np.array([])
-    rh = np.array([])
-    w = np.array([])
-    d = np.array([])
-    v = np.array([])
-    thta = np.array([])
-    thte = np.array([])
-    thtv = np.array([])
+	p = np.array([])
+	h = np.array([])
+	t = np.array([])
+	td = np.array([])
+	rh = np.array([])
+	w = np.array([])
+	d = np.array([])
+	v = np.array([])
+	thta = np.array([])
+	thte = np.array([])
+	thtv = np.array([])
 
-    for i in range(len(lines)):
-        dummy = lines[i].split()
-        p    = np.pad(p,(0,1),'constant',constant_values = float(dummy[0]))
-        h    = np.pad(h,(0,1),'constant',constant_values = float(dummy[1]))
-        t    = np.pad(t,(0,1),'constant',constant_values = float(dummy[2]))
-        td   = np.pad(td,(0,1),'constant',constant_values = float(dummy[3]))
-        rh   = np.pad(rh,(0,1),'constant',constant_values = float(dummy[4]))
-        w    = np.pad(w,(0,1),'constant',constant_values = float(dummy[5]))
-        d    = np.pad(d,(0,1),'constant',constant_values = float(dummy[6]))
-        v    = np.pad(v,(0,1),'constant',constant_values = float(dummy[7]))
-        thta = np.pad(thta,(0,1),'constant',constant_values = float(dummy[8]))
-        thte = np.pad(thte,(0,1),'constant',constant_values = float(dummy[9]))
-        thtv = np.pad(thtv,(0,1),'constant',constant_values = float(dummy[10]))
-
-
-    plt.figure()
-    plt.axis([mintemp,maxtemp,1000,np.min(p)])
-    plt.plot(t,p,label='TEMP')
-    plt.plot(td,p,label='DWPT')
-    plt.yscale('log')
-    plt.xlabel('Temperature in C')
-    plt.ylabel('Log Pressure in hPa')
-    plt.legend()
-    plt.grid(True)
-    plt.title('Skew T log P')
+	for i in range(len(lines)):
+		dummy = lines[i].split()
+		p    = np.pad(p,(0,1),'constant',constant_values = float(dummy[0]))
+		h    = np.pad(h,(0,1),'constant',constant_values = float(dummy[1]))
+		t    = np.pad(t,(0,1),'constant',constant_values = float(dummy[2]))
+		td   = np.pad(td,(0,1),'constant',constant_values = float(dummy[3]))
+		rh   = np.pad(rh,(0,1),'constant',constant_values = float(dummy[4]))
+		w    = np.pad(w,(0,1),'constant',constant_values = float(dummy[5]))
+		d    = np.pad(d,(0,1),'constant',constant_values = float(dummy[6]))
+		v    = np.pad(v,(0,1),'constant',constant_values = float(dummy[7]))
+		thta = np.pad(thta,(0,1),'constant',constant_values = float(dummy[8]))
+		thte = np.pad(thte,(0,1),'constant',constant_values = float(dummy[9]))
+		thtv = np.pad(thtv,(0,1),'constant',constant_values = float(dummy[10]))
 
 
-    plt.figure()
-    plt.axis([mintemp,maxtemp,280,np.max(thta)])
-    plt.plot(t,thta,label='TEMP')
-    plt.plot(td,thta,label='DWPT')
-    plt.xlabel('Temperature in C')
-    plt.ylabel('Potentail temperature in K')
-    plt.legend()
-    plt.grid(True)
-    plt.title('Tephigram')
+	plt.figure()
+	plt.axis([mintemp,maxtemp,1000,np.min(p)])
+	plt.plot(t,p,label='TEMP')
+	plt.plot(td,p,label='DWPT')
+	plt.yscale('log')
+	plt.xlabel('Temperature in C')
+	plt.ylabel('Log Pressure in hPa')
+	plt.legend()
+	plt.grid(True)
+	plt.title('Skew T log P')
 
-    plt.show()
+
+	plt.figure()
+	plt.axis([mintemp,maxtemp,280,np.max(thta)])
+	plt.plot(t,thta,label='TEMP')
+	plt.plot(td,thta,label='DWPT')
+	plt.xlabel('Temperature in C')
+	plt.ylabel('Potentail temperature in K')
+	plt.legend()
+	plt.grid(True)
+	plt.title('Tephigram')
+
+	plt.show()
