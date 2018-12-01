ac71fbdb75017cebfffa90931f74fd410ae43a5d
kvngvikram <happykvng@gmail.com>
2018-12-01 23:01:41 +0530

    added PIL image show in imd code

--- imd.py
+++ imd.py
@@ -7,8 +7,12 @@ address = 'http://satellite.imd.gov.in/img/Thiruvanantapurum.gif'
 
 
 import requests
+from PIL import Image
 
-f = open('image.gif','wb')
+f = open('temp_image.gif','wb')
 f.write(requests.get(address).content)
 f.close()
+
+im = Image.open('temp_image.gif')
+im.show()
 
