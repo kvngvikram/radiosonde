a93211181ce7b1dc7dc148b39ec62e1f5dbb098f
kvngvikram <happykvng@gmail.com>
2018-12-01 23:34:52 +0530

    improved imd script with no temperary image file

--- imd.py
+++ imd.py
@@ -2,17 +2,17 @@
 # For any isseus please contact KVNG Vikram
 
 
-
+# Adress of sonder image for required location can by selected from the map http://satellite.imd.gov.in/map_skm2.html 
+# use that address below (Use http:// )
 address = 'http://satellite.imd.gov.in/img/Thiruvanantapurum.gif'
 
 
 import requests
 from PIL import Image
 
-f = open('temp_image.gif','wb')
-f.write(requests.get(address).content)
-f.close()
 
-im = Image.open('temp_image.gif')
+response = requests.get(address,stream=True).raw
+
+im = Image.open(response)
 im.show()
 
